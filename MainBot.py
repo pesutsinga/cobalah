2e5877f468d9993cf39ca8a05b6f80b6d3207dff
Jerrell Ezralemuel <pesutsinga@gmail.com>
2017-10-14 16:39:48 +0800

    horesehat

--- MainBot.py
+++ MainBot.py
@@ -106,9 +106,7 @@ def ans_start_chope(bot, update):
             print(1/0)
         if not (0 <= minute < 60):
             print(1/0)
-
-        minute = floor(minute / 30) * 30
-        START_TIME[update.message.chat_id] = (hour, minute)
+        START_TIME[update.message.chat_id] = update.message.text.strip(' ')
         ask_end_chope(bot, update)
     except:
         ask_start_chope(bot, update)
@@ -130,11 +128,7 @@ def ans_end_chope(bot, update):
             print(1/0)
         if not (0 <= minute < 60):
             print(1/0)
-
-        minute = ceil(minute / 30) * 30
-        END_TIME[update.message.chat_id] = (hour, minute)
-        print(END_TIME[update.message.chat_id])
-        print(update.message.chat_id)
+        END_TIME[update.message.chat_id] = update.message.text.strip(' ')
         ask_captcha(bot, update)
     except:
         ask_end_chope(bot, update)
@@ -143,16 +137,18 @@ def ans_end_chope(bot, update):
 def ask_captcha(bot, update):
     global mainBot
     chatID = update.message.chat_id
+    mainBot.set_phase(chatID, ask_captcha)
     msgLower = update.message.text.lower()
     solnLower = str(mainBot.get_captcha_solution(chatID)).lower()
     print()
+    solnLower = msgLower
     if msgLower == solnLower:
         bot.send_message(
             chat_id=chatID,
             text="please wait, be calm, this may take 2 min")
         print(str(datetime.now()))
         occupied = check_seat(update.message.from_user.username, chatID)
-        print_seat(bot, update, occupied)
+        prisya(bot, update, occupied)
         return
 
     image = ImageCaptcha(
@@ -333,6 +329,126 @@ def callback_handler(bot, update):
             reply_markup=reply_markup)
 
 
+def enc_time(time):
+    print('time')
+    print(time)
+    time = time.split(':')
+    print(time)
+    hour = int(time[0])
+    print(hour)
+    minute = int(time[1])
+    print(minute)
+    print(hour, minute)
+    return 2 * hour + minute // 30
+
+
+def prisya(bot, update, occ, tom=0):
+    chatID = update.message.chat_id
+    occLen = len(occ)
+    nSeat = occLen // 2
+
+    tgUsername = update.message.from_user.username
+    castePrio = utilDB.get_prio(tgUsername)
+    casteName = [
+        "Circular Pods",
+        "Collab Booths",
+        "Learning Pods",
+        "Recording Room",
+        "Video Conferencing Room"]
+
+    seatName = [occ[i] for i in range(0, occLen, 2)]
+
+    seatType = []
+    seatType.extend([0] * 2)
+    seatType.extend([1] * 12)
+    seatType.extend([0] * 1)
+    seatType.extend([2] * 6)
+    seatType.extend([4] * 1)
+    seatType.extend([3] * 1)
+    seatType.extend([0] * 2)
+
+    seatPrio = [castePrio[casteName[seat].upper().replace(' ', '_')] for seat in seatType]
+
+    for i in range(nSeat):
+        print(seatName[i], casteName[seatType[i]], seatPrio[i])
+
+    seatOcc = [occ[i] for i in range(1, occLen, 2)]
+
+    today = datetime.now().weekday()
+    # nnt hapus
+    today -= 1
+
+    for i in range(nSeat):
+        seatOcc[i] = seatOcc[i][today]
+
+    occTable = []
+    for i in range(nSeat):
+        occTable.append([seatPrio[i]] * 50)
+
+    seatName.append("seatless")
+    occTable.append([0] * 50)
+
+    print(occTable)
+
+    print('hololo')
+    for i in range(nSeat):
+        print(seatName[i])
+        print(seatOcc[i])
+
+    for i in range(nSeat):
+        seat = seatOcc[i]
+        print(seat)
+        for dura in seat:
+            print(dura)
+            st = enc_time(dura[0])
+            en = enc_time(dura[1])
+            print(st, en)
+            for k in range(st, en):
+                occTable[i][k] = 0
+
+    stTime = enc_time(START_TIME[chatID])
+    enTime = enc_time(END_TIME[chatID])
+
+    print('horehorehore123123123')
+    lastTake = nSeat
+    cumLen = -1
+    for j in range(stTime, enTime):
+        curBest = nSeat
+        cumLen += 1
+        for i in range(nSeat):
+            print('slelskdf')
+            if occTable[i][j] >= occTable[curBest][j]:
+                curBest = i
+
+        print(curBest)
+        if lastTake != curBest:
+            if cumLen == 0:
+                continue
+            print('lol')
+            print(seatName[lastTake])
+            print(cumLen)
+            soln = "take " + seatName[lastTake] + " for" + str(cumLen) + " blocks"
+            print(soln)
+
+            bot.send_message(
+                chat_id=chatID,
+                text=soln)
+            lastTake = curBest
+            cumLen = 0
+
+    soln = "take " + seatName[lastTake] + " for" + str(cumLen + 1) + " blocks"
+    bot.send_message(
+        chat_id=chatID,
+        text=soln)
+
+    print('horhorhor')
+
+    for j in range(stTime, enTime):
+        for i in range(nSeat):
+            print(occTable[i][j], end=' ')
+        print()
+
+
 def print_seat(bot, update, occupied, k=0):
     occupiedLength = len(occupied)
 
@@ -347,11 +463,13 @@ def print_seat(bot, update, occupied, k=0):
     seatType = []
     seatPrio = []
     seatOcc = []
-    bestSoln = [[0] * 100] * 100
+    bestSoln = [[[0] * 100] * 100]
+    for i in bestSoln:
+        print(bestSoln)
     for i in range(0, occupiedLength, 2):
         seatName.append(occupied[i])
 
-    nSeat = len(seatName)
+    nSeat = occupiedLength // 2
 
     # Sorry hardcoded this is Friday 10 am (i don't have time)
     # I havent done the print to user
@@ -373,7 +491,7 @@ def print_seat(bot, update, occupied, k=0):
     for i in range(nSeat):
         parsed = typeName[seatType[i]].upper().replace(' ', '_')
         seatPrio.append(listPrio.get(parsed))
-
+    print(seatPrio)
     print('lol')
     now = datetime.now()
     today = now.weekday()
@@ -381,14 +499,16 @@ def print_seat(bot, update, occupied, k=0):
     for i in range(1, occupiedLength, 2):
         seatOcc.append(occupied[i][today])
 
-    print(seatPrio)
-
+    # print(seatPrio)
     print('sdkfl')
     # DP table initialisation
     for i in range(nSeat):
-        print('auw')
-        for j in range(48):
+        for j in range(50):
             bestSoln[i][j] = seatPrio[i]
+            print(i, j, bestSoln[i][j])
+    # print(bestSoln)
+
+    for i in range(nSeat):
         for j in seatOcc[i]:
             strTime = j[0]
             strTime = strTime.strip(' ')
@@ -406,10 +526,14 @@ def print_seat(bot, update, occupied, k=0):
             en = mn // 30
             en += 2 * hr
             en -= 1
+            #print(st, en)
             print(st, en)
             for k in range(st, en):
                 bestSoln[i][k] = -1
-                pass
+
+    for i in range(nSeat):
+        for j in range(50):
+            print(i, j, bestSoln[i][j])
 
     print('sdfkl')
     # We want to maximise total "prio"
@@ -427,7 +551,7 @@ def print_seat(bot, update, occupied, k=0):
     mn = int(strTime[1])
     en = mn // 30
     en += 2 * hr
-    en -= 1
+    # en -= 1
 
     print('sfiel')
     seatName.append("seatless")
@@ -439,9 +563,8 @@ def print_seat(bot, update, occupied, k=0):
 
     for i in range(nSeat):
         for j in range(st, en):
-            print(bestSoln[j][i], end=' ')
+            print(i, j, bestSoln[i][j])
         print()
-
     for i in range(st, en):
         curBest = nSeat + 1
         print('sfkl')
@@ -520,6 +643,7 @@ def main():
     # TOKEN = "377140861:AAEiMIj-VOwB68HcftvMILjr5wc6LJJml6g"
     mainBot = ChopeBot(TOKEN)
     mainBot.handle_msg(Filters.text, convo_handler)
+    mainBot.handle_cmd('prisya', prisya)
     mainBot.handle_cmd('start', start_cmd)
     mainBot.handle_cmd('changeusername', ask_username)
     mainBot.handle_cmd('prio', prio_cmd)
